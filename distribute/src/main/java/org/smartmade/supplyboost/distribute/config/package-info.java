f1f9bab028978b4045b08e73ce91b5d4672b070d
Saeid Amini <amini.com@gmail.com>
2022-10-16 17:10:51 +0330

    Initial version of Distribute

---
+++ distribute/src/main/java/org/smartmade/supplyboost/distribute/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package org.smartmade.supplyboost.distribute.config;
---
+++ distribute/src/test/java/org/smartmade/supplyboost/distribute/IntegrationTest.java
@@ -0,0 +1,22 @@
+package org.smartmade.supplyboost.distribute;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.smartmade.supplyboost.distribute.DistributeApp;
+import org.smartmade.supplyboost.distribute.config.AsyncSyncConfiguration;
+import org.smartmade.supplyboost.distribute.config.EmbeddedSQL;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { DistributeApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
